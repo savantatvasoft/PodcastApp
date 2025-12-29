4a31495a4405b8a689f99fd3f1f104fdc098e473
savantatvasoft <savan.soni@tatvasoft.com>
2025-12-29 17:04:26 +0530

    Update folder structure and setup color

---
+++ PodcastApp/Features/PodcastDiscovery/Views/PodcastDiscoveryVC.swift
@@ -0,0 +1,29 @@
+//
+//  PodcastDiscoveryVC.swift
+//  PodcastApp
+//
+//  Created by MACM72 on 29/12/25.
+//
+
+import UIKit
+
+class PodcastDiscoveryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
